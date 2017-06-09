0d404154625e5ff5de399e5433dfb8f00c46d22d
anasauce <anasauceda1843@gmail.com>
2017-06-09 16:15:51 -0700

    adds express API endpoint for organization signup, no testing at this moment

--- routes/organization/index.js
+++ routes/organization/index.js
@@ -1,20 +1,20 @@
 const express = require( 'express' )
 const router = express.Router()
 const passport = require( '../../configuration/passport' )
-const User = require( '../../database/user' )
+const organization = require( '../../database/commands/organization' )
 
-router.post( '/organization-signup', ( request, response ) => {
+router.post( '/signup', ( request, response ) => {
   const newOrganization = {
     user_id: request.user.id,
     name: request.body.name,
     logo_url: request.body.logo_url,
     website: request.body.website,
   }
-
-
-
-  //.then(function that adds this data to the database)
-  //.then(respond with json and with 201 )
-  //.catch( error )
-
+  organization.create( newOrganization )
+  .then( results => {
+    response.json(results[0])
+  })
+  .catch( error => {
+    response.render( 'Error signing up organization: ', { error } )
+  })
 })
